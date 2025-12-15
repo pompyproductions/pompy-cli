d446d2c1fe124725d05251cf191153cbba153c4b
Pompy Productions <eren@pompyproductions.com>
2025-12-15 15:51:00 +0100

    Add ANSI styles and padding

--- src/pompy_cli/prompt.py
+++ src/pompy_cli/prompt.py
@@ -1,6 +1,11 @@
+from style import style, Ansi
+
 def prompt_str(text=str, case_insensitive=False):
     print(text)
-    user_input = input("(Enter any string.) ")
+    user_input = input(style("(Enter any string.) ", color=Ansi.FG_CYAN))
     if case_insensitive:
         return user_input.lower()
     return user_input
+
+if __name__ == "__main__":
+    print("Your name is " + style(prompt_str("What's your name?", True), color=Ansi.BG_YELLOW, decoration=Ansi.BOLD, padding=1))
